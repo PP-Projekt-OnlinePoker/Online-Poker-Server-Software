d9aac9fcfe5f15610aab2c46e6beec4056409292
Schlechtweg Till <dqi12schlechtweg@szut.uni-bremen.de>
2016-01-12 10:49:25 +0100

    Event changed

--- src/de/szut/dqi12/holdem/helper/Event.java
+++ src/de/szut/dqi12/holdem/helper/Event.java
@@ -11,7 +11,8 @@ public class Event {
 	public static final String EVENT_TABLEID = "tableId";
 	public static final String EVENT_COMMWAY = "commway";
 	
-	public static final String 
+	public static final String EVENT_USERNAME = "username";
+	public static final String EVENT_PASSWORD = "password";
 	
 	private Integer tableId;
 	private Integer playerId;
@@ -48,13 +49,26 @@ public class Event {
 	}
 	
 	public String getJSONString(){
+		JSONObject obj =new JSONObject();
 		switch(this.type){
 		case LOGIN:
-			JSONObject obj = new JSONObject;
-			obj.put(EVENT_PLAYERNAME, this.player.getUserName);
-			obj.put(EVENT_PASSWORD, this.player.getPassword);
+			obj.put(EVENT_USERNAME, this.username);
+			obj.put(EVENT_PASSWORD, this.password);
+			break;
+		case BET:
+			break;
+		case CHECK:
+			break;
+		case FOLD:
+			break;
+		case RAISE:
+			break;
+		case TABLELIST:
+			break;
+		default:
 			break;
 		}
+		return obj.toString();
 	}
 	
 	
