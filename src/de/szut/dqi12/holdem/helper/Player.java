74e3b5c75c285b75cdb9f055b6a1bdee7af4746b
Mehmet <lordkakpuu@gmail.com>
2016-01-19 10:42:00 +0100

    Database-Handler finished

---
+++ src/de/szut/dqi12/holdem/helper/Player.java
@@ -0,0 +1,79 @@
+package de.szut.dqi12.holdem.helper;
+
+public class Player {
+
+	public Integer playerId;
+	public Integer money;
+	public String username;
+	public String vorname;
+	public String nachname;
+	public String eMail;
+	public String password;
+	
+	/**
+	 * Login constructor
+	 * @param username
+	 * @param password
+	 */
+	public Player(String username, String password){
+		this.username = username;
+		this.password = password;
+	}
+
+	public Integer getPlayerId() {
+		return playerId;
+	}
+
+	public void setPlayerId(Integer playerId) {
+		this.playerId = playerId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getVorname() {
+		return vorname;
+	}
+
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+
+	public String getNachname() {
+		return nachname;
+	}
+
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+
+	public String geteMail() {
+		return eMail;
+	}
+
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public Integer getMoney() {
+		return money;
+	}
+
+	public void setMoney(Integer money) {
+		this.money = money;
+	}
+
+}
