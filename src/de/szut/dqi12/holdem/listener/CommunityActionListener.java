1e0f6f3cfaed9e5f5f9c8e2b3474d19e44b7de25
dqi12schlechtweg <dqi12schlechtweg@W127_04.szut.uni-bremen.de>
2016-02-02 09:03:49 +0100

    Updated the master branch

--- src/de/szut/dqi12/holdem/listener/CommunityActionListener.java
+++
@@ -1,8 +0,0 @@
-package de.szut.dqi12.holdem.listener;
-
-import de.szut.dqi12.holdem.helper.Event;
-
-public interface CommunityActionListener {
-	public void onLogin(Event e);
-	public void onGetProfile(Event e);
-}
